96a0e068bb45510c7355058012245dde0e9185d7
Levi Duquette <lsduquette@uncg.edu>
2026-04-09 13:32:17 -0400

    updated webcontroller

--- src/main/java/com/example/character_api/CharacterWebController.java
+++ src/main/java/com/example/character_api/CharacterWebController.java
@@ -1,5 +1,65 @@
 package com.example.character_api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller // this is @Controller, not @RestController, because we are returning view names, not JSON future me
+@RequestMapping("/mvc/characters") 
 public class CharacterWebController {
-    
+
+    private final CharacterService characterService;
+
+    public CharacterWebController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+    // 1. GET ALL (character-list.ftlh)
+    @GetMapping
+    public String viewAllCharacters(Model model) {
+        model.addAttribute("characterList", characterService.getAllCharacters());
+        return "character-list"; 
+    }
+
+    // 2. GET ONE (character-details.ftlh)
+    @GetMapping("/{id}")
+    public String viewCharacterDetails(@PathVariable Long id, Model model) {
+        model.addAttribute("character", characterService.getCharacterById(id));
+        return "character-details";
+    }
+
+    // 3. SHOW CREATE FORM (character-create.ftlh)
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        return "character-create";
+    }
+
+    // 4. PROCESS CREATE FORM (list or homepage)
+    @PostMapping("/new")
+    public String processCreateForm(Character character) {
+        characterService.addCharacter(character);
+        return "redirect:/mvc/characters";
+    }
+
+    // 5. SHOW UPDATE FORM (character-update.ftlh)
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable Long id, Model model) {
+        model.addAttribute("character", characterService.getCharacterById(id));
+        return "character-update";
+    }
+
+    // 6. PROCESS UPDATE FORM (.details)
+    @PostMapping("/update/{id}")
+    public String processUpdateForm(@PathVariable Long id, Character character) {
+        characterService.updateCharacter(id, character);
+        return "redirect:/mvc/characters/" + id;
+    }
+
+    // 7. DELETE
+    @GetMapping("/delete/{id}")
+    public String deleteCharacter(@PathVariable Long id) {
+        characterService.deleteCharacter(id);
+        return "redirect:/mvc/characters";
+    }
 }
+    
